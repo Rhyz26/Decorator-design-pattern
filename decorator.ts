ab35c4993d907a73b520af7b24e06fe81730571f
Raymond <njagala.raymond@gmail.com>
2024-11-01 16:27:48 +0300

    organising the code

--- decorator.ts
+++ decorator.ts
@@ -1,56 +1,55 @@
-// component interface 
+// component interface
 interface Coffee {
-    cost(): number;
-    description(): string;
+  cost(): number;
+  description(): string;
 }
 
 //CONCRETE COMPONENTS
 class SimpleCoffee implements Coffee {
-    cost(): number {
-        return 5;
-    }
+  cost(): number {
+    return 5;
+  }
 
-    description(): string{
-        return "Simple coffee";
-    }
+  description(): string {
+    return "Simple coffee";
+  }
 }
 
 // BASE DECORATOR
-abstract class CoffeeDecorator implements Coffee{
-    protected coffee: Coffee;
+abstract class CoffeeDecorator implements Coffee {
+  protected coffee: Coffee;
 
-    constructor(coffee: Coffee){
-        this.coffee = coffee;
-    }
+  constructor(coffee: Coffee) {
+    this.coffee = coffee;
+  }
 
-    cost(): number{
-        return this.coffee.cost();
-    }
+  cost(): number {
+    return this.coffee.cost();
+  }
 
-    description(): string {
-        return this.coffee.description();
-    }
+  description(): string {
+    return this.coffee.description();
+  }
 }
 
 // CONCRETE DECORATORS
 
-class MilkDecorator extends CoffeeDecorator{
-    cost(): number{
-        return this.coffee.cost() + 1;
-    }
-    description(): string {
-        return this.coffee.description() + " " + "milk";
-    }
-
+class MilkDecorator extends CoffeeDecorator {
+  cost(): number {
+    return this.coffee.cost() + 1;
+  }
+  description(): string {
+    return this.coffee.description() + " " + "milk";
+  }
 }
 
-class CaramelDecorator extends CoffeeDecorator{
-    cost(): number{
-        return this.coffee.cost() + 2;
-    }
-    description(): string {
-        return this.coffee.description() + " + caramel"
-    }
+class CaramelDecorator extends CoffeeDecorator {
+  cost(): number {
+    return this.coffee.cost() + 2;
+  }
+  description(): string {
+    return this.coffee.description() + " + caramel";
+  }
 }
 
 // Using our decorators
@@ -63,9 +62,7 @@ myCoffee = new MilkDecorator(myCoffee);
 console.log(myCoffee.description());
 console.log(myCoffee.cost());
 
-
 // Add Caramel
 myCoffee = new CaramelDecorator(myCoffee);
 console.log(myCoffee.description());
 console.log(myCoffee.cost());
-
